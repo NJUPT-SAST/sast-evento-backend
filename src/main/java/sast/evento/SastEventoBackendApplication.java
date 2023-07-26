5a708640008281498c4e250cb7d648ac61890645
FeelMoose <feell4429@163.com>
2023-07-26 19:48:31 +0800

    fix：MapperScan

--- src/main/java/sast/evento/SastEventoBackendApplication.java
+++ src/main/java/sast/evento/SastEventoBackendApplication.java
@@ -1,11 +1,12 @@
 package sast.evento;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("sast.evento.mapper")
 public class SastEventoBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SastEventoBackendApplication.class, args);
 	}
