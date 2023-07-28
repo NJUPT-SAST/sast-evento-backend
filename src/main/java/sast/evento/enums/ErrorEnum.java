154d518de3d8c8a0ff781edc2e878811f61ed332
FeelMoose <feell4429@163.com>
2023-07-28 12:35:44 +0800

    feat：修改ErrorEnum

--- src/main/java/sast/evento/enums/ErrorEnum.java
+++ src/main/java/sast/evento/enums/ErrorEnum.java
@@ -11,12 +11,29 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorEnum {
+    /* 错误 */
     COMMON_ERROR(1000, "错误"),
-    METHOD_NOT_EXIST(1001, "方法不存在"),
-    TOKEN_ERROR(1002,"TOKEN解析错误"),
-    PERMISSION_ERROR(1003,"权限错误"),
-    USER_ALREADY_EXIST(1004,"用户已存在"),
-    STATEMENT_ERROR(1005,"权限statement错误"),
+    /* 鉴权错误 */
+    PERMISSION_ERROR(1001,"权限错误"),
+    USER_ALREADY_EXIST(1002,"用户已存在"),
+    TOKEN_ERROR(1003,"TOKEN解析错误"),
+    METHOD_NOT_EXIST(1004, "该操作不存在"),
+    /*  */
+
+
+
+
+
+
+
+
+
+
+
+    /* 其他服务错误 */
+    WX_SUBSCRIBE_ERROR(10001,"微信订阅错误"),
+    SCHEDULER_ERROR(10002,"scheduler错误"),
+    QRCODE_ERROR(10003,"二维码生成错误")
     ;
     private final Integer errCode;
     private final String errMsg;
