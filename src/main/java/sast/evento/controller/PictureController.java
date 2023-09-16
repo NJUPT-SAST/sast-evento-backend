4faf81c749d3cb4ff1e81d308676a11d009a89bc
FeelMoose <feell4429@163.com>
2023-09-16 16:53:33 +0800

    feat 图片假分页

--- src/main/java/sast/evento/controller/PictureController.java
+++ src/main/java/sast/evento/controller/PictureController.java
@@ -1,7 +1,6 @@
 package sast.evento.controller;
 
 import com.qcloud.cos.exception.CosClientException;
-import com.qcloud.cos.exception.CosServiceException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import sast.evento.annotation.DefaultActionState;
@@ -9,13 +8,10 @@ import sast.evento.annotation.OperateLog;
 import sast.evento.common.enums.ActionState;
 import sast.evento.common.enums.ErrorEnum;
 import sast.evento.exception.LocalRunTimeException;
-import sast.evento.response.GlobalResponse;
 import sast.evento.utils.CosUtil;
-import sast.sastlink.sdk.exception.SastLinkException;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @projectName: sast-evento-backend
@@ -25,32 +21,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/picture")
 public class PictureController {
+
     @OperateLog("获取所有目录")
-    @DefaultActionState(value = ActionState.ADMIN,group = "picture")
+    @DefaultActionState(value = ActionState.ADMIN, group = "picture")
     @GetMapping("/dir")
     public List<String> getDir(@RequestParam(defaultValue = "") String dir) {
         try {
             return CosUtil.getDirs(dir);
         } catch (CosClientException e) {
-            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR,e.getMessage());
+            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR, e.getMessage());
         }
     }
 
     @OperateLog("获取图片url列表")
-    @DefaultActionState(value = ActionState.ADMIN,group = "picture")
+    @DefaultActionState(value = ActionState.ADMIN, group = "picture")
     @GetMapping("/list")
-    public List<String> getUrls(@RequestParam(defaultValue = "") String dir,
-                                @RequestParam(defaultValue = "") String lastUrl,
-                                @RequestParam(defaultValue = "20") Integer size) {
+    public Map<String, Object> getUrls(@RequestParam(defaultValue = "") String dir,
+                                       @RequestParam(defaultValue = "1") Integer num,
+                                       @RequestParam(defaultValue = "20") Integer size) {
         try {
-            return CosUtil.getURLs(dir, lastUrl, size);
+            List<String> keys = CosUtil.getKeys(dir);
+            List<String> res = CosUtil.changeKey2URL(keys.subList(num - 1, num + size - 1));
+            return Map.of("pictures", CosUtil.changeKey2URL(res), "total", keys.size());
         } catch (CosClientException e) {
-            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR,e.getMessage());
+            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR, e.getMessage());
         }
     }
 
     @OperateLog("添加图片")
-    @DefaultActionState(value = ActionState.ADMIN,group = "picture")
+    @DefaultActionState(value = ActionState.ADMIN, group = "picture")
     @PostMapping("/info")
     public String addPicture(MultipartFile picture,
                              @RequestParam(defaultValue = "") String dir) {
@@ -60,14 +59,14 @@ public class PictureController {
         try {
             return CosUtil.upload(picture, dir);
         } catch (CosClientException e) {
-            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR,e.getMessage());
+            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR, e.getMessage());
         } catch (Exception e) {
-            throw new LocalRunTimeException(ErrorEnum.COMMON_ERROR,e.getMessage());
+            throw new LocalRunTimeException(ErrorEnum.COMMON_ERROR, e.getMessage());
         }
     }
 
     @OperateLog("删除图片")
-    @DefaultActionState(value = ActionState.ADMIN,group = "picture")
+    @DefaultActionState(value = ActionState.ADMIN, group = "picture")
     @DeleteMapping("/info")
     public String deletePicture(@RequestParam String url) {
         if (url.isEmpty()) {
@@ -76,7 +75,7 @@ public class PictureController {
         try {
             CosUtil.delete(url);
         } catch (CosClientException e) {
-            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR,e.getMessage());
+            throw new LocalRunTimeException(ErrorEnum.COS_SERVICE_ERROR, e.getMessage());
         }
         return "ok";
     }
--- src/main/java/sast/evento/utils/CosUtil.java
+++ src/main/java/sast/evento/utils/CosUtil.java
@@ -17,6 +17,7 @@ import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -72,7 +73,31 @@ public class CosUtil {
                 .toList();
     }
 
-    /* 获取当前桶下所有目录 */
+    /* 获取当前目录下所有key(无限制) */
+    public static List<String> getKeys(String dir) throws CosClientException {
+        String dirPrefix = dir.isEmpty() ? "" : (dir + "/");
+        List<String> res = new ArrayList<>();
+        ObjectListing objectListing;
+        String marker = null;
+        do {
+            ListObjectsRequest request = new ListObjectsRequest(bucketName, dirPrefix, marker, "/", 1000);
+            objectListing = cosClient.listObjects(request);
+            List<String> every = objectListing.getObjectSummaries().stream()
+                    .map(COSObjectSummary::getKey)
+                    .toList();
+            res.addAll(every);
+            marker = objectListing.getNextMarker();
+        } while (marker != null);
+        return res;
+    }
+
+    public static List<String> changeKey2URL(List<String> keys) {
+        return keys.stream()
+                .map(key -> "https://" + bucketName + ".cos." + COS_REGION + ".myqcloud.com/" + key)
+                .toList();
+    }
+
+    /* 获取当前桶下1000以内所有目录 */
     public static List<String> getDirs(String dir) throws CosClientException {
         String dirPrefix = dir.isEmpty() ? "" : (dir + "/");
         ListObjectsRequest request = new ListObjectsRequest(bucketName, dirPrefix, "", "/", 1000);
